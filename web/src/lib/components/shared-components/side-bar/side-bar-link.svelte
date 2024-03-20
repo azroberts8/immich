1d2b68f584b3ce88def572f0488fe741952faa44
Andrew Roberts <andrew@azroberts.com>
2024-03-20 11:52:19 -0400

    Fixed spacing of sidebar buttons

--- web/src/lib/components/shared-components/side-bar/side-bar-link.svelte
+++ web/src/lib/components/shared-components/side-bar/side-bar-link.svelte
@@ -22,7 +22,7 @@
   data-sveltekit-preload-data={preloadData ? 'hover' : 'off'}
   draggable="false"
   aria-current={isSelected ? 'page' : undefined}
-  class="flex w-full place-items-center justify-between gap-4 rounded-r-full py-3 transition-[padding] delay-100 duration-100 hover:cursor-pointer hover:bg-immich-gray hover:text-immich-primary dark:text-immich-dark-fg dark:hover:bg-immich-dark-gray dark:hover:text-immich-dark-primary
+  class="flex w-full place-items-center justify-between gap-4 rounded-r-full py-2 transition-[padding] delay-100 duration-100 hover:cursor-pointer hover:bg-immich-gray hover:text-immich-primary dark:text-immich-dark-fg dark:hover:bg-immich-dark-gray dark:hover:text-immich-dark-primary
     {isSelected
     ? 'bg-immich-primary/10 text-immich-primary hover:bg-immich-primary/25 dark:bg-immich-dark-primary/10 dark:text-immich-dark-primary'
     : ''}
